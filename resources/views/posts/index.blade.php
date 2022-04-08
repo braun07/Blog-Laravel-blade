e08a3b8a09a2efb553e9be2a1149eec1ef76d96e
braun07 <joaovitorbraun6@gmail.com>
2022-04-08 13:55:27 -0300

    Autenticação para likes e posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,24 +5,25 @@
         <div class="w-8/12 bg-white p-6 rounded-lg">
             <form action="{{ route('posts') }}" method="post" class="mb-4">
                 @csrf
-                <div class="mb-4">
-                    <label for="body" class="sr-only">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="4" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post Something!"></textarea>
+                @auth
+                    <div class="mb-4">
+                        <label for="body" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4" 
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post Something!"></textarea>
 
-                    @error('body')
-                        <div class="text-red-500 mt-2 text-sm">
-                            <p>You need to text something</p>
-                        </div>
-                    @enderror
-                </div>
+                        @error('body')
+                            <div class="text-red-500 mt-2 text-sm">
+                                <p>You need to text something</p>
+                            </div>
+                        @enderror
+                    </div>
 
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
-                        Post
-                    </button>
-                </div>
-
+                    <div>
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
+                            Post
+                        </button>
+                    </div>
+                @endauth
             </form>
 
             @if($posts->count())
@@ -34,18 +35,20 @@
                         <p class="mb-2">{{ $post->body }}</p>
 
                         <div class="flex items-center">
-                            @if (!$post->likedBy(auth()->user()))
-                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Like</button>
-                                </form>
-                            @else
-                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-blue-500">Unlike</button>
-                                </form>
-                            @endif
+                            @auth
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth
 
                             <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                         </div>
