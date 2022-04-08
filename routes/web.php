b10248c38d6034f7295ba63b939be88d1f0037f1
braun07 <joaovitorbraun6@gmail.com>
2022-04-08 17:00:02 -0300

    Implementação da pagina pessoal de cada post

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/registest', [registestController::class, 'index'])->name('registest
 Route::post('/registest', [registestController::class, 'store']);
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
